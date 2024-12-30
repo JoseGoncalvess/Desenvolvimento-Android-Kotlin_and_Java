587849afddb02c1c41d0ef07b05f7785b24e861e
JoseGoncalvess <joseinacio774@gmail.com>
2024-12-30 14:37:48 -0300

    Fragmente usage

---
+++ Conteudo/FragmentsAction/app/src/main/java/com/jgdev/fragmentsaction/fragments/ConverssasFragmet.kt
@@ -0,0 +1,19 @@
+package com.jgdev.fragmentsaction.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jgdev.fragmentsaction.R
+
+class ConverssasFragmet : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_converssas, container, false)
+
+    }
+}
