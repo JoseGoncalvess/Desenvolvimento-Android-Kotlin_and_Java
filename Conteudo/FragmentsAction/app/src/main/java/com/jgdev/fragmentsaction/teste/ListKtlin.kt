6458104803f8a226b80c5684451d887a7166348a
JoseGoncalvess <joseinacio774@gmail.com>
2025-01-14 14:05:52 -0300

    ListCLass

---
+++ Conteudo/FragmentsAction/app/src/main/java/com/jgdev/fragmentsaction/teste/ListKtlin.kt
@@ -0,0 +1,25 @@
+package com.jgdev.fragmentsaction.teste
+
+// TODO LIST QUE APOS SER CRIADA NÂO È POSSIVEL TER SUA ESTRUTURA ALTERADA
+val imutableList = mutableListOf<Int>(1,8,25)
+
+// TODO ESSA MESMO APÓS SUA CRIAÇÂO PODE SER ALTERADA
+val mutableList = mutableListOf<String>("1","8","25")
+
+
+fun main() {
+
+    // TODO METODOS LIST PARA MUTABLE LIST
+    mutableList.add("COCA")
+    mutableList.remove("8")
+    mutableList.size
+    mutableList.first()
+    mutableList.last()
+
+    mutableList.get(0)
+    mutableList.addAll(listOf("COCADA","SABÂO","MARACUJA"))
+
+    mutableList.forEach { it -> println(it) }
+
+    // TODO OS ETODOS DE "ADD" "REMOVE" "clear"
+}
