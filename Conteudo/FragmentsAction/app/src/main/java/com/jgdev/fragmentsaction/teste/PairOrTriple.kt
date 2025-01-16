529aa0848cddbde7e3129e0132718f543a9ee4f4
JoseGoncalvess <joseinacio774@gmail.com>
2025-01-16 14:10:19 -0300

    Pair and Triple

---
+++ Conteudo/FragmentsAction/app/src/main/java/com/jgdev/fragmentsaction/teste/PairOrTriple.kt
@@ -0,0 +1,20 @@
+package com.jgdev.fragmentsaction.teste
+
+
+fun main() {
+    // TODO DUAS ETSRUTURAS QUE PODEM SER APLICADOS A CASOS DE USO
+
+    // PAIR - ARMAZENA DOIS VALORES DYNAMIC
+    val localization = Pair(-12561558487,-12564895875)
+    println(localization.first)
+    println(localization.second)
+
+
+
+    // TRIPLE - ARMAZENA DOIS VALORES DYNAMIC
+    val bandeira = Triple("BRASIL","BR","AMERICA LATINA")
+    println(bandeira.first)
+    println(bandeira.second)
+    println(bandeira.third)
+
+}
