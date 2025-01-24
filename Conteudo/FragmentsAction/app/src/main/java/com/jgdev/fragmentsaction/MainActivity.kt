5b9edf9f069f14de63d0b84c7480f8c42c19d05c
JoseGoncalvess <joseinacio774@gmail.com>
2025-01-24 16:31:05 -0300

    Introduct RecycleView

--- Conteudo/FragmentsAction/app/src/main/java/com/jgdev/fragmentsaction/MainActivity.kt
+++ Conteudo/FragmentsAction/app/src/main/java/com/jgdev/fragmentsaction/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rv_lista)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- Conteudo/FragmentsAction/app/src/main/java/com/jgdev/fragmentsaction/RecycleViewActivity.kt
+++ Conteudo/FragmentsAction/app/src/main/java/com/jgdev/fragmentsaction/RecycleViewActivity.kt
@@ -1,37 +1,25 @@
 package com.jgdev.fragmentsaction
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
-class ListViewActivity : AppCompatActivity() {
-    private  lateinit var  listViewUSer: ListView;
-
+class RecycleViewActivity : AppCompatActivity() {
+    lateinit var  rv_list : RecyclerView;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_list_view)
+        setContentView(R.layout.activity_recycle_view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rv_lista)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-        val users = listOf("Gonçlaves","Anny","Gui","Maria","Guilherme","Maykon")
-
-        listViewUSer = findViewById(R.id.listview_user)
-
-        listViewUSer.adapter = ArrayAdapter(
-            this,
-            android.R.layout.simple_list_item_activated_2,
-            android.R.id.text1,
-            users
-        )
+        rv_list = findViewById(R.id.rv_lista)
 
     }
 }
