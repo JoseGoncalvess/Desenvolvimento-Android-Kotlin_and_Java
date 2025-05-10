4089c2b42eae6d4f5d923c41f365a834f75cd060
José Gonçalves <joseinacio774@gmail.com>
2025-05-10 17:30:07 -0300

    Learn Fab Extends

--- Conteudo/ViewBiding/app/src/main/java/com/jgdev/viewbiding/content/FloatingActionButtonActivity.kt
+++ Conteudo/ViewBiding/app/src/main/java/com/jgdev/viewbiding/content/FloatingActionButtonActivity.kt
@@ -1,21 +1,38 @@
 package com.jgdev.viewbiding.content
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.transition.Visibility
 import com.jgdev.viewbiding.R
+import com.jgdev.viewbiding.databinding.ActivityFloatingActionButtonBinding
+import com.jgdev.viewbiding.databinding.ActivityMainBinding
 
 class FloatingActionButtonActivity : AppCompatActivity() {
+    private  val binding  by lazy {
+        ActivityFloatingActionButtonBinding.inflate(layoutInflater)
+    };
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_floating_action_button)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        with(binding){
+                FabButton.setOnClickListener{
+                   if(groupMenu.isVisible){
+                       groupMenu.visibility = View.INVISIBLE
+                   }
+                    groupMenu.visibility = View.VISIBLE
+                }
         }
     }
 }
