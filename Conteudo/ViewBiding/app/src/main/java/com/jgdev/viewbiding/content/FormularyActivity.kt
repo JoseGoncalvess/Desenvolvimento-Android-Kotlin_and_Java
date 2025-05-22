b809e5e8f3c235ee6d2ae5dda120877b0571cc12
José Gonçalves <joseinacio774@gmail.com>
2025-05-21 21:46:15 -0300

    Learn SnackABar

--- Conteudo/ViewBiding/app/src/main/java/com/jgdev/viewbiding/content/FormularyActivity.kt
+++ Conteudo/ViewBiding/app/src/main/java/com/jgdev/viewbiding/content/FormularyActivity.kt
@@ -1,10 +1,15 @@
 package com.jgdev.viewbiding.content
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 import com.jgdev.viewbiding.R
 import com.jgdev.viewbiding.databinding.ActivityFormularyBinding
 
@@ -23,12 +28,25 @@ ActivityFormularyBinding.inflate(layoutInflater)
         }
 
         with(biding){
-//            btnEnviar.setOnClickListener {
+            btnEnviar.setOnClickListener {
 //                atualizar()
-//            }
-awitchToggle()
+//                awitchToggle()
+                showSnackBar(it)
+            }
 
         }
+    }
+
+    private fun showSnackBar(view: View) {
+             val snakBar = Snackbar.make(
+                view,"Rustinn!!!",Snackbar.LENGTH_LONG
+                )
+        snakBar.setAction("OPA",{
+            Toast.makeText(this,"Feito",Toast.LENGTH_LONG).show()
+        })
+        snakBar.setBackgroundTint(ContextCompat.getColor(this, R.color.black),)
+        snakBar.setActionTextColor(ContextCompat.getColor(this,android.R.color.holo_purple))
+
     }
 
     private fun awitchToggle() {
