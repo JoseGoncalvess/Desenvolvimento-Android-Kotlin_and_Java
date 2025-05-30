f66f3aec83bda0eecebad727176a288ecebceccc
JoseGoncalvess <joseinacio774@gmail.com>
2025-05-30 16:01:07 -0300

    ajustr for Snack bar

--- Conteudo/ViewBiding/app/src/main/java/com/jgdev/viewbiding/content/FormularyActivity.kt
+++ Conteudo/ViewBiding/app/src/main/java/com/jgdev/viewbiding/content/FormularyActivity.kt
@@ -26,20 +26,32 @@ ActivityFormularyBinding.inflate(layoutInflater)
         with(biding){
 //            btnEnviar.setOnClickListener {
 //                atualizar()
-//            }
-
-            chekedTerm.setOnCheckedChangeListener { compoundButton, isCheked ->
-                val resultado = if (isCheked) "SIm" else "Não"
-                biding.textResult.text = "O CHEKEDE ESTÀ $resultado"
+//                awitchToggle()
+                showSnackBar(it)
             }
 
-           val rgIdade =  rgIdade.checkedRadioButtonId
-            textResult.text = when(rgIdade){
-                R.id.rbIdade -> "15"
-                R.id.rbIdade2 -> "25"
-                else -> "NADA SELECIOANDO"
-            }
         }
+    }
+
+    private fun showSnackBar(view: View) {
+        val snakBar = Snackbar.make(
+            view,"Rustinn!!!",Snackbar.LENGTH_LONG
+        )
+        snakBar.setAction("OPA",{
+            Toast.makeText(this,"Feito",Toast.LENGTH_LONG).show()
+        })
+        snakBar.setBackgroundTint(ContextCompat.getColor(this, R.color.black),)
+        snakBar.setActionTextColor(ContextCompat.getColor(this,android.R.color.holo_purple))
+
+    }
+
+    private fun awitchToggle() {
+        val switchMarcado = biding.switchNotification.isChecked
+        val toggleMarcado = biding.toggleCont.isChecked
+
+        val text = "Switch: $switchMarcado | Tooggle: $toggleMarcado"
+
+        biding.textResult.text = text
     }
 
     private fun atualizar() {
